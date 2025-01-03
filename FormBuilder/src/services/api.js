fbecb9d7747998492aa07f614332845e8776e1aa
imAshishSaini <19bcs1363@gmail.com>
2025-01-03 23:54:22 +0530

    update

--- FormBuilder/src/services/api.js
+++ FormBuilder/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://formbot-backend-1-81ij.onrender.com',
 })
 
 API.interceptors.request.use((req) => {
